3ecbdf56b2e5eacbc0b4174a643d758aaf9ef291
saujas <saujas.adarkar@gmail.com>
2018-12-02 00:05:08 +0530

    Adapter, ether, ip finished, and reformatting

--- code.pl
+++ code.pl
@@ -2,74 +2,119 @@
 
 packet(X, Y, Z) :-
     allow(A, B, C),
-    validate_adapter(X, A), 
-    validate_ethernet(Y, B),
+    testAdapter(X, A), 
+    testEthernet(Y, B),
     testIP(Z, C).
 
 
-testIP([A|[B|_]], [E|[F|_]]) :-
-    A=E,
-    B=F.
+testAdapter(X, L) :-
+    \+X='any',
+    split_string(L, "-", "", T),
+    length(T, I),
+    I=2,
+    memberOfRange(X,T).
+    
+testAdapter(X, L) :-
+    \+X='any',
+    split_string(L, ",", "", T),
+    length(T, I),
+    I >= 2,
+    memberOfList(X, T).
 
-testIP([A, B], [E, F]) :-
-    rangeIP(E, A),  
-    B=F.
+testAdapter(X, P) :-
+    \+X='any',
+    string_length(P, I),
+    I=1,
+    char_code(X, XC),
+    char_code(P, PC),
+    XC=PC.
 
-testIP([A, B], [E, F]) :-
-    mask(E, A),
-    B=F.
+testAdapter(X, A) :-
+    X=A,
+    A='any'.
 
-testIP([A, B], [E, F]) :-
-    rangeIP(F, B),  
+testEthernet([V|[P|_]], [X, Y]) :-
+    testVid(V, X),
+    testProto(P, Y).
+
+testVid(X, E) :-
+    split_string(E, "-", "", T),
+    length(T, I),
+    I=2,
+    memberOfNumberRange(X,T).
+
+testVid(X, E) :-
+    split_string(E, ",", "", T),
+    length(T, I),
+    I >= 2,
+    memberOfNumberList(X, T).
+
+testVid(X, E) :-
+    string_to_list(E, S),
+    checkValidNumber(S),
+    atom_number(E, EN),
+    X=EN.
+
+testVid(X, E) :-
+    X=E,
+    E='null'.
+
+
+testProto(X, E) :-
+    split_string(E, "-", "", T),
+    length(T, I),
+    I=2,
+    memberOfNumberRange(X,T).
+
+testProto(X, E) :-
+    split_string(E, ",", "", T),
+    length(T, I),
+    I >= 2,
+    memberOfNumberList(X, T).
+
+testProto(X, E) :-
+    string_to_list(E, S),
+    checkValidNumber(S),
+    atom_number(E, EN),
+    X=EN.
+
+testProto(X, E) :-
+    X=E,
+    E='null'.
+
+
+
+testIP([A|[B|C]], [E|[F|G]]) :-
+    srcIP(A, E),
+    dstIP(B, F),
+    C=G.
+
+
+srcIP(A, E) :-
     A=E.
 
-testIP([A, B], [E, F]) :-
-    mask(F, B),
-    A=E.
+srcIP(A, E) :-
+    mask(E, A).
 
-testIP([A, B], [E, F]) :-
-    rangeIP(E, A),
+srcIP(A, E) :-
+    rangeIP(E, A).
+
+srcIP(A, E) :-
+    commaIP(E, A).
+
+
+dstIP(B, F) :-
+    B=F.
+
+dstIP(B, F) :-
     rangeIP(F, B).
 
-testIP([A, B], [E, F]) :-
-    rangeIP(E, A), 
+dstIP(B, F) :-
     mask(F, B).
 
-testIP([A, B], [E, F]) :-
-    mask(E, A),
-    mask(F, B).
-
-testIP([A, B], [E, F]) :-
-    mask(E, A),
-    rangeIP(F, B).
-
-testIP([A, B], [E, F]) :-
-    A=E,
+dstIP(B, F) :-
     commaIP(F, B).
 
-testIP([A, B], [E, F]) :-
-    rangeIP(E, A), 
-    commaIP(F, B).
-
-testIP([A, B], [E, F]) :-
-    mask(E, A),
-    commaIP(F, B).
-
-testIP([A, B], [E, F]) :-
-    B=F,
-    commaIP(E, A).
-
-testIP([A, B], [E, F]) :-
-    rangeIP(F, B),
-    commaIP(E, A).
-
-testIP([A, B], [E, F]) :-
-    mask(F, B),
-    commaIP(E, A).
-
-testIP([A, B], [E, F]) :-
-    commaIP(E, A),
-    commaIP(F, B).
 
 commaIP(X, A) :-
     split_string(X, ",", "", L),
@@ -78,20 +123,15 @@ commaIP(X, A) :-
 
 
 mask(X, B) :-
-    split_string(B, "/", "", L),
     X=B,
+    split_string(B, "/", "", L),
     lastEl(C, L), 
     C=<32,
     C>=1.
 
 
-check([L1, L2, L3 | _], [M1, M2, M3 | _]) :-
-    L1=M1,
-    L2=M2, 
-    L3=M3.
-
 rangeIP(X, I) :-
-    split_string(X, "-", "", L), 
+    split_string(X, "-", "", L),
     splitNo(L, I).
 
 splitNo([A, B], I) :-
@@ -101,7 +141,7 @@ splitNo([A, B], I) :-
     lastEl(Q, L),
     lastEl(R, M),
     lastEl(S, N), 
-    memberofNumberAsString(Q, R, S, L, M).  
+    memberofNumberAsString(Q, R, S, L, N).  
 
 lastEl(X,[X]).
 
@@ -117,93 +157,6 @@ memberofNumberAsString(Q, R, S, [L1, L2, L3 | _], [M1, M2, M3 | _]) :-
     number_codes(Z, S), 
     Z>=X, 
     Z=<Y.
-
-
-
-validate_adapter(X, L) :-
-    \+X='any',
-    %allow(L, _, _),
-    split_string(L, "-", "", T),
-    length(T, I),
-    I=2,
-    memberOfRange(X,T).
-    
-validate_adapter(X, L) :-
-    \+X='any',
-    %allow(L, _, _),
-    split_string(L, ",", "", T),
-    length(T, I),
-    I >= 2,
-    memberOfList(X, T).
-
-validate_adapter(X, P) :-
-    \+X='any',
-    %allow(P, _, _),
-    string_length(P, I),
-    I=1,
-    char_code(X, XC),
-    char_code(P, PC),
-    XC=PC.
-
-validate_adapter(X, A) :-
-    X=A,
-    A='any'.
-
-validate_ethernet([V|[P|_]], [X, Y]) :-
-    validate_vid(V, X),
-    validate_proto(P, Y).
-
-validate_vid(X, E) :-
-    %allow(_, [E|_], _),
-    split_string(E, "-", "", T),
-    length(T, I),
-    I=2,
-    memberOfNumberRange(X,T).
-
-validate_vid(X, E) :-
-    %allow(_, [E|_], _),
-    split_string(E, ",", "", T),
-    length(T, I),
-    I >= 2,
-    memberOfNumberList(X, T).
-
-validate_vid(X, E) :-
-    %allow(_, [E|_], _),
-    string_to_list(E, S),
-    checkValidNumber(S),
-    atom_number(E, EN),
-    X=EN.
-
-validate_vid(X, E) :-
-    X=E,
-    E='null'.
-
-
-validate_proto(X, E) :-
-    %allow(_, [_|[E|_]], _),
-    split_string(E, "-", "", T),
-    length(T, I),
-    I=2,
-    memberOfNumberRange(X,T).
-
-validate_proto(X, E) :-
-    %allow(_, [_|[E|_]], _),
-    split_string(E, ",", "", T),
-    length(T, I),
-    I >= 2,
-    memberOfNumberList(X, T).
-
-validate_proto(X, E) :-
-    %allow(_, [_|[E|_]], _),
-    string_to_list(E, S),
-    checkValidNumber(S),
-    atom_number(E, EN),
-    X=EN.
-
-validate_proto(X, E) :-
-    X=E,
-    E='null'.
-
 
 
 memberOfList(_, []) :-
