4c34f1b463796989b75f7182ae8a4f612af93a0c
Ujjwal Gandhi <ujjwalgandhi@yahoo.com>
2018-12-02 10:26:09 +0530

    Commenting started

--- code.pl
+++ code.pl
@@ -1,5 +1,8 @@
 :- consult(db).
 
+%!!---------MULTIPLE TYPES OF RESPONSES FOR THE PACKET THROUGH THE FIREWALL---------!!
+
+%The packet is allow to go through when it matches with an allow rule in the database. An acceptance message is printed.
 packet(X, Y, Z, W, T) :-
     allow(A, B, C, D, E),
     testAdapter(X, A), 
@@ -9,6 +12,7 @@ packet(X, Y, Z, W, T) :-
     testTcpUdp(T, E),
     write("Packet accepted!").
 
+%The packet is rejected when it matches with a reject rule in the database, printing a rejection message.
 packet(X, Y, Z, W, T) :-
     reject(A, B, C, D, E),
     testAdapter(X, A), 
@@ -18,6 +22,7 @@ packet(X, Y, Z, W, T) :-
     testTcpUdp(T, E),
     write("Packet rejected!"), false.
 
+%The packet is dropped silently when a corresponding drop rule exists in the database. Nothing is printed in this case.
 packet(X, Y, Z, W, T) :-
     drop(A, B, C, D, E),
     testAdapter(X, A), 
@@ -27,6 +32,11 @@ packet(X, Y, Z, W, T) :-
     testTcpUdp(T, E),
     false.
 
+
+%!!---------TESTING THE FIRST CLAUSE OF THE PACKET, THE ADAPTER CLAUSE---------!!
+
+%The adapter matches when it is found in adapter clause's 'hyphen' separated rule in the database.
+%Examples - 'A-C', 'B-F'
 testAdapter(X, L) :-
     \+X='any',
     split_string(L, "-", "", T),
@@ -34,6 +44,8 @@ testAdapter(X, L) :-
     I=2,
     memberOfRange(X,T).
     
+%The adapter matches when it is found in adapter clause's'comma' separated rule in the database.
+%Examples - 'L,M', 'S,T,Y'
 testAdapter(X, L) :-
     \+X='any',
     split_string(L, ",", "", T),
@@ -41,6 +53,8 @@ testAdapter(X, L) :-
     I >= 2,
     memberOfList(X, T).
 
+%The adapter matches when it is found in adapter clause's single character rule in the database.
+%Examples - 'A-C', 'B-F'
 testAdapter(X, P) :-
     \+X='any',
     string_length(P, I),
@@ -49,31 +63,97 @@ testAdapter(X, P) :-
     char_code(P, PC),
     XC=PC.
 
+%The adapter matches when it is not meant to have a clause. That happens when 'any' is given as an input to the adapter clause in the packet.
 testAdapter(X, A) :-
     X=A,
     A='any'.
 
+
+%!!---------TESTING THE SECOND CLAUSE OF THE PACKET, THE ETHERNET CLAUSE---------!!
+
+%The clause is satisfied only when both the elements of the list (vid and proto) satisfy corresponding predicates - testVid and testProto writing afterwards.
 testEthernet([V|[P|_]], [X, Y]) :-
     testVid(V, X),
     testProto(P, Y).
 
+testVid(X, E) :-
+    split_string(E, "-", "", T),
+    length(T, I),
+    I=2,
+    memberOfNumberRange(X,T).
+
+testVid(X, E) :-
+    split_string(E, ",", "", T),
+    length(T, I),
+    I >= 2,
+    memberOfNumberList(X, T).
+
+testVid(X, E) :-
+    string_to_list(E, S),
+    checkValidNumber(S),
+    atom_number(E, EN),
+    X=EN.
+
+testVid(X, E) :-
+    X=E,
+    E='null'.
+
+
+testProto(X, E) :-
+    split_string(E, "-", "", T),
+    length(T, I),
+    I=2,
+    memberOfNumberRange(X,T).
+
+testProto(X, E) :-
+    split_string(E, ",", "", T),
+    length(T, I),
+    I >= 2,
+    memberOfNumberList(X, T).
+
+testProto(X, E) :-
+    string_to_list(E, S),
+    checkValidNumber(S),
+    atom_number(E, EN),
+    X=EN.
+
+testProto(X, E) :-
+    X=E,
+    E='null'.
+
+
+%!!---------TESTING THE FOURTH CLAUSE OF THE PACKET, THE ICMP CLAUSE---------!!
+
+%The clause is satisfied only when both the elements of the list (test and code) satisfy corresponding predicates.
+%Since they work on the same principles as testVid and testProto, they must be satisfied here as well.
 testICMP([V|[P|_]], [X, Y]) :-
     testVid(V, X),
     testProto(P, Y).
 
+
+%!!---------TESTING THE FIFTH CLAUSE OF THE PACKET, THE TCP/UDP CLAUSE---------!!
+
+%The clause is satisfied when source and destination addresses (first and second elements of the input list) match
+%AND
+%The third elements corresponds to either 'tcp' or 'udp'. 
 testTcpUdp([X, Y, Z], [A, B, C]) :-
     testSrcDst(X, A),
     testSrcDst(Y, B),
     testName(Z, C).
 
+%Tests for the third element of TCP/UDP clause to be 'udp'
 testName(A, B) :-
     A=B,
     B='udp'.
 
+%Tests for the third element of TCP/UDP clause to be 'tcp'
 testName(A, B) :-
     A=B,
-    B='tdp'.
+    B='tcp'.
 
+%Tests the source and destination addresses of TCP/UDP when any (or both) of them match(es) a 'hyphen' separated rule in the database.
+%Tests if the input is in the correct range as per the Internet Assigned Numbers Authority.
+%Examples - 192.168.1.1-15
 testSrcDst(X, E) :-
     split_string(E, "-", "", T),
     length(T, I),
@@ -82,6 +162,9 @@ testSrcDst(X, E) :-
     X=<65535,
     X>=0.
 
+%Tests the source and destination addresses of TCP/UDP when any (or both) of them match(es) a 'comma' separated rule in the database.
+%Tests if the input is in the correct range as per the Internet Assigned Numbers Authority.
+%Examples - 192.168.1.1,192.168.1.15
 testSrcDst(X, E) :-
     split_string(E, ",", "", T),
     length(T, I),
@@ -90,7 +173,8 @@ testSrcDst(X, E) :-
     X=<65535,
     X>=0.
 
-
+%Tests the source and destination addresses of TCP/UDP when any (or both) of them match(es) a simple rule - without 'hyphen'/'comma' in the database.
+%Tests if the input is in the correct range as per the Internet Assigned Numbers Authority.
 testSrcDst(X, E) :-
     string_to_list(E, S),
     checkValidNumber(S),
@@ -99,55 +183,12 @@ testSrcDst(X, E) :-
     X=<65535,
     X>=0.
 
+%Tests the source and destination addresses of TCP/UDP when any (or both) of them are not meant to have an input.
+%In our rulebook, this happens when the keyword 'null' is used in the place of the address.
+%This checks the input for that 'null' case.
 testSrcDst(X, E) :-
     X=E,
     E='null'.
-
-testVid(X, E) :-
-    split_string(E, "-", "", T),
-    length(T, I),
-    I=2,
-    memberOfNumberRange(X,T).
-
-testVid(X, E) :-
-    split_string(E, ",", "", T),
-    length(T, I),
-    I >= 2,
-    memberOfNumberList(X, T).
-
-testVid(X, E) :-
-    string_to_list(E, S),
-    checkValidNumber(S),
-    atom_number(E, EN),
-    X=EN.
-
-testVid(X, E) :-
-    X=E,
-    E='null'.
-
-
-testProto(X, E) :-
-    split_string(E, "-", "", T),
-    length(T, I),
-    I=2,
-    memberOfNumberRange(X,T).
-
-testProto(X, E) :-
-    split_string(E, ",", "", T),
-    length(T, I),
-    I >= 2,
-    memberOfNumberList(X, T).
-
-testProto(X, E) :-
-    string_to_list(E, S),
-    checkValidNumber(S),
-    atom_number(E, EN),
-    X=EN.
-
-testProto(X, E) :-
-    X=E,
-    E='null'.
-
 
 
 testIP([A|[B|C]], [E|[F|G]]) :-
